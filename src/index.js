3e5c5a0bdbb37a8c33332107b1eb7d1716e43d78
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-04-04 00:18:31 +0530

    - rendered HomePage

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import React from "react";
 // import ReactDOM from "react-dom";
 // const render = ReactDOM.render;
 import { render } from "react-dom";
+import HomePage from "./components/HomePage";
+
+render(<HomePage />, document.getElementById("root"));
