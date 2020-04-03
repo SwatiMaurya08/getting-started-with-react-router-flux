a6c2a29463d69f24b5825eec17752ccffdba4749
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-04-04 00:39:52 +0530

    -created aboutus component using class & react-fragment

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    //React.Fragment
+    return (
+      <>
+        <h2>About Us</h2>
+        <p>This app uses React.</p>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
