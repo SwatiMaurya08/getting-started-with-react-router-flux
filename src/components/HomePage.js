d3032571d94076d1ec2c6c371a9f12874c0a10e3
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-04-03 22:49:24 +0530

    - created react component

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { defaults } from "json-server";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Plularsight Administration</h1>
+      <p>React. Flux, and Reatc Router for ultra-response web app</p>
+    </div>
+  );
+}
+
+export default HomePage;
