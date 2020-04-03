2de64203bef77a913a125bf29990967c16eb91ec
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-04-03 22:54:00 +0530

    - remove uncessary import

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { defaults } from "json-server";
 
 function HomePage() {
   return (
     <div className="jumbotron">
       <h1>Plularsight Administration</h1>
-      <p>React. Flux, and Reatc Router for ultra-response web app</p>
+      <p>React. Flux, and React Router for ultra-response web app</p>
     </div>
   );
 }
