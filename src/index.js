80369b5ffab4307cea428a6dc919d8b7e2f56472
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-04-04 00:41:01 +0530

    -changed render file to About us

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import React from "react";
 // const render = ReactDOM.render;
 import { render } from "react-dom";
 import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
 
-render(<HomePage />, document.getElementById("root"));
+render(<AboutPage />, document.getElementById("root"));
