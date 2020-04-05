095b740cde63b731c6d3e57af29ff4e756a0a430
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-04-05 20:36:44 +0530

    - get course api

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { getCourses } from "../api/courseApi";
 
 class CoursesPage extends React.Component {
+  state = {
+    courses: [],
+  };
+
+  componentDidMount() {
+    // using anonymous function
+    // getCourses().then(function (mockCourse) {
+    //   this.setState({ courses: mockCourse });
+    // });
+
+    // using arrow function
+    getCourses().then((mockCourse) => this.setState({ courses: mockCourse }));
+  }
+
   render() {
     return <h2>Courses</h2>;
   }
